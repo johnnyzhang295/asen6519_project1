722abf77e4a496afcd8fc9bbae16277b43daf225
Johnny Zhang <johnny.zhang916@gmail.com>
2020-02-11 19:26:07 -0700

    jamies updated code

--- Proj1.py
+++ Proj1.py
@@ -70,7 +70,7 @@ class AOI:
 
         
 #intput data on our AOIs
-A = AOI(2,4,2,0,1,1,5,'A')
+A = AOI(2,4,2,0,1,1,4,'A')
 B = AOI(3,2,1,1,0,3,6,'B')
 C = AOI(1,3,1,1,3,0,4,'C')
 D = AOI(2,1,5,5,6,4,0,'D')
@@ -87,12 +87,12 @@ for i in range (0,10):
     #we are now inbetween
     states.append('between')
     #how long are we inbetween for?
-    t.append(t[-1]+np.random.lognormal(0,0.5))
+    t.append(t[-1]+np.random.normal(0.03,0.003))
     #pick next state
     state = state.stochnext()
     states.append(state.name)
 
-plt.step(t,states, where = 'post')
+#plt.step(t,states, where = 'post')
 
 def montecarlo(fixations = 100, start = A):
     t = 0
@@ -104,8 +104,9 @@ def montecarlo(fixations = 100, start = A):
     'D': 0
        }
     for i in range(0,fixations):
-        t += np.random.lognormal(0,0.5)
         #how long are we inbetween for?
+        t += np.random.normal(0.03,0.003)
+        #how long are we at this state for?
         tplus = np.random.lognormal(0,0.5)
         t+=tplus
         myStates[state.name] += tplus
@@ -114,3 +115,52 @@ def montecarlo(fixations = 100, start = A):
         
     myStates = {k: v / t for k, v in myStates.items()}
     return myStates
+
+count = 0
+for i in range(0,1000):
+    count +=1
+    myStates = montecarlo()
+    try:
+        mytotalStates = {k: v+myStates[k] for k, v in mytotalStates.items()}
+    except:
+        mytotalStates = myStates
+
+mytotalStates = {k: v / count for k, v in mytotalStates.items()}
+
+print(mytotalStates)
+
+def WindTime(start = A):
+    t = np.random.lognormal(0,0.5) #how much time did we spend on starting state
+    state = start    
+    noticed = False
+    while not noticed:
+        t+=np.random.normal(0.03,0.003) #time spent inbetween
+        state = state.stochnext() #this is where we go next
+        tplus = np.random.lognormal(0,0.5) #this is how long we spend there
+        if state == D and t > 10 and np.random.random()>0.8:
+            noticed= True
+            t += np.random.random()*tplus
+            break
+        elif state == D and t + tplus > 10 and np.random.random()>0.8:
+            noticed= True
+            t = 10 + (t+ tplus - 10)*np.random.random()
+            break
+        else:
+            t+=tplus
+            
+
+        if t>1000:
+            break
+        
+    return t
+
+
+times = []
+for i in range(0,1000):
+    times.append(WindTime(A))
+m = round(np.median(times),2)
+CI = [round(np.percentile(times,5),2), round(np.percentile(times,95),2)]
+plt.hist(times)
+title = 'mean: ' + str(m) + '. 5th - 95th percentile: '+ str(CI) + '.'
+plt.title(title)
+
